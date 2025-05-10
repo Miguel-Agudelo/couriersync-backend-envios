9cfde9cee7335e9d6a067a9b33c85457e1a45799
Miguel Agudelo <veramiguel348@gmail.com>
2025-05-10 12:34:49 -0500

    subiendo proyecto Spring Boot generado con Spring Initializr

---
+++ src/main/java/com/couriersync/backendenvios/CouriersyncBackendEnviosApplication.java
@@ -0,0 +1,13 @@
+package com.couriersync.backendenvios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouriersyncBackendEnviosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouriersyncBackendEnviosApplication.class, args);
+	}
+
+}
